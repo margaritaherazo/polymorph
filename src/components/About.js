ef0b624264c7162e3988e5eb677fe734cec60bc8
maggi <margarita.herazo.i@gmail.com>
2024-09-16 13:58:22 +0200

    example about section

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+// src/components/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Us</h2>
+      <p>Learn more about our mission, vision, and the team behind our therapies.</p>
+      {/* Add more content about the team or the mission here */}
+    </section>
+  );
+}
+
+export default About;
