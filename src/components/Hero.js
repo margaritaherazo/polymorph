0cd3ca319d4c5f476c00a408617e96937aac700e
maggi <margarita.herazo.i@gmail.com>
2024-09-16 13:57:06 +0200

    example hore section

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+// src/components/Hero.js
+import React from 'react';
+
+function Hero() {
+  return (
+    <section id="hero">
+      <h1>Welcome to Our Therapy Website</h1>
+      <p>Find the perfect therapy for you and start your journey to wellness.</p>
+      {/* Add more hero section content here */}
+    </section>
+  );
+}
+
+export default Hero;
