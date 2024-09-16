cacadd0f4de3de242d2291fbef46de077e139f82
maggi <margarita.herazo.i@gmail.com>
2024-09-16 13:59:46 +0200

    importing hero, concept and about sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
-// src/pages/Home.js
+
 import React from 'react';
-import Contact from '../components/Contact';
+import Hero from '../components/Hero';  
+import Concept from '../components/Concept';  
+import About from '../components/About';  
+import Contact from '../components/Contact';  
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to Our Therapy Website</h1>
-      <p>Explore the different therapies we offer and learn more about how they can help you.</p>
-
+      <Hero />
+      <Concept />
+      <About />
       <Contact />
-
     </div>
   );
 }
 
 export default Home;
+
