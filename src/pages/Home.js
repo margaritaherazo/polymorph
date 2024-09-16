b583af01189a3c36f5b4004db3ed34785c2ada67
maggi <margarita.herazo.i@gmail.com>
2024-09-16 13:56:30 +0200

    importing contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 // src/pages/Home.js
 import React from 'react';
+import Contact from '../components/Contact';
 
 function Home() {
   return (
     <div>
       <h1>Welcome to Our Therapy Website</h1>
       <p>Explore the different therapies we offer and learn more about how they can help you.</p>
+
+      <Contact />
+
     </div>
   );
 }
