36fd29e67a22fb3051e8641739b922ef1062c0d3
maggi <margarita.herazo.i@gmail.com>
2024-09-16 13:57:49 +0200

    example concept section

---
+++ src/components/Concept.js
@@ -0,0 +1,22 @@
+// src/components/Concept.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Concept() {
+  return (
+    <section id="concept">
+      <h2>Our Concept</h2>
+      <p>Learn more about our approach and the therapies we offer.</p>
+      <ul>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/familyconst">Family Constellations</Link></li>
+        <li><Link to="/fascialmanuvers">Fascial Manuvers</Link></li>
+        <li><Link to="/tre">TRE</Link></li>
+        <li><Link to="/vocalbreath">Vocal Breath Therapy</Link></li>
+      </ul>
+      {/* Add photos or other content here */}
+    </section>
+  );
+}
+
+export default Concept;
