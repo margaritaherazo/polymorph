eaae346d7de0105abce356d09e8cce6ef7510329
maggi <margarita.herazo.i@gmail.com>
2024-09-17 14:11:31 +0200

    adding space btwn li element Navbar and adding img for hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
     <section id="hero">
       <h1>Welcome to Our Therapy Website</h1>
       <p>Find the perfect therapy for you and start your journey to wellness.</p>
-      {/* Add more hero section content here */}
+      <img src="src\assets\imgs\coper.jpeg" alt="Fascial" className="hero-image" />
     </section>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <div className="header-area">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <Link className="navbar-brand" to="/">Logo</Link>
+            <Link className="navbar-brand" to="/">Polymorph</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -17,7 +17,7 @@ function Navbar() {
                 <li className="nav-item"><Link className="nav-link" to="/familyconst">Family Constellations</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/fascialmanuvers">Fascial Maneuvers</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/tre">TRE</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/vocalbreath">Vocal Breath</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/vocalbreath">Vocal&Breath</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="#contact">Contact</Link></li>
               </ul>
             </div>
