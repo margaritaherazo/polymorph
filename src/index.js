3d46633441e514b06a0dfd06fe52d3c8a78caf97
maggi <margarita.herazo.i@gmail.com>
2024-09-17 12:22:17 +0200

    organizing better styles files and adding Navbar.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';  
-import './index.css';
+import './styles/App.css';  
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
