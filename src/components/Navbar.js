8bacb22926ec3b86a4b9f7028da56c3bfb87d83b
maggi <margarita.herazo.i@gmail.com>
2024-09-17 13:19:42 +0200

    adjusting navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ import '../styles/Navbar.css';
 function Navbar() {
   return (
     <header className="top-area">
-    <div className="header-area">
-      <nav className="navbar navbar-default bootsnav navbar-fixed dark no-background">
-        <div className="container">
-          <div className="navbar-header">
-            <Link className="navbar-brand" to="/">Logo</Link> 
-          </div>
-          <div className="collapse navbar-collapse menu-ui-design" id="navbar-menu">
-            <ul className="nav navbar-nav navbar-right">
+      <div className="header-area">
+        <nav className="navbar navbar-default bootsnav navbar-fixed dark no-background">
+          <div className="container">
+            <div className="navbar-header">
+              <Link className="navbar-brand" to="/">Logo</Link>
+            </div>
+            <div className="navbar-collapse" id="navbar-menu">
+              <ul className="nav navbar-nav navbar-right">
                 <li><Link className="smooth-menu" to="/">Home</Link></li>
                 <li><Link className="smooth-menu" to="/familyconst">Family Constellations</Link></li>
                 <li><Link className="smooth-menu" to="/fascialmanuvers">Fascial Maneuvers</Link></li>
                 <li><Link className="smooth-menu" to="/tre">TRE</Link></li>
                 <li><Link className="smooth-menu" to="/vocalbreath">Vocal Breath</Link></li>
-                <li><Link className="smooth-menu" to="#contact">Contact</Link></li> 
+                <li><Link className="smooth-menu" to="#contact">Contact</Link></li>
               </ul>
             </div>
           </div>
