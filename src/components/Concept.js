7282425543c49c81603d23934ce65b95b5075fe5
maggi <margarita.herazo.i@gmail.com>
2024-09-18 10:52:52 +0200

    addings example imgs

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -6,13 +6,14 @@ function Concept() {
   return (
     <section id="concept">
       <h2>Our Concept</h2>
-      <p>Learn more about our approach and the therapies we offer.</p>
-      <ul>
-        <li><Link to="/familyconst">Family Constellations</Link></li>
-        <li><Link to="/fascialmanuvers">Fascial Manuvers</Link></li>
-        <li><Link to="/tre">TRE</Link></li>
-        <li><Link to="/vocalbreath">Vocal Breath Therapy</Link></li>
-      </ul>
+      <p>Learn more about our approach and the therapies we offer.
+       we are working in trauma blbalabalabaal
+      <Link to="/familyconst">Family Constellations</Link> and then blalabalaba
+      <Link to="/fascialmanuvers">Fascial Manuvers</Link> and blabalbalaa
+      <Link to="/tre">TRE</Link> and also in our vocal etc
+        <Link to="/vocalbreath">Vocal Breath Therapy</Link>
+      
+      </p>
       {/* Add photos or other content here */}
     </section>
   );
