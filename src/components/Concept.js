e034cbe3d05ec1b678fb6709db82db57e1273856
maggi <margarita.herazo.i@gmail.com>
2024-09-18 11:06:14 +0200

    setingup concept component

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 
 function Concept() {
   return (
-    <section id="concept">
-      <h2>Our Concept</h2>
+    <section id="concept" className="container">
+      <h2 className="text-center">Our Concept</h2>
+      <div className="row">
+        <div className="col-md-6">
       <p>Learn more about our approach and the therapies we offer.
        we are working in trauma blbalabalabaal
       <Link to="/familyconst">Family Constellations</Link> and then blalabalaba
@@ -14,7 +16,23 @@ function Concept() {
         <Link to="/vocalbreath">Vocal Breath Therapy</Link>
       
       </p>
-      {/* Add photos or other content here */}
+      </div>
+      
+      <div className="col-md-6">
+        <div className="image-item">
+          <img src="/assets/imgs/family.jfif" alt="Family Constellations" />
+        </div>
+        <div className="image-item">
+          <img src="/assets/imgs/fascial.jfif" alt="Fascial Manuvers" />
+        </div>
+        <div className="image-item">
+          <img src="/assets/imgs/tre.jpg" alt="TRE" />
+        </div>
+        <div className="image-item">
+          <img src="/assets/imgs/breath.jfif" alt="Vocal Breath Therapy" />
+        </div>
+      </div>
+      </div>
     </section>
   );
 }
