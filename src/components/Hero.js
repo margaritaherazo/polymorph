80eaacfd50cc1ed941b1fe35ccfadb90a67ee53d
maggi <margarita.herazo.i@gmail.com>
2024-09-18 10:40:16 +0200

    setting up img and styles hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,6 @@ function Hero() {
     <section id="hero">
       <h1>Welcome to Our Therapy Website</h1>
       <p>Find the perfect therapy for you and start your journey to wellness.</p>
-      <img src="../assets/imgs/coper.jpeg" alt="Fascial" className="hero-image" />
     </section>
   );
 }
