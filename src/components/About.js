00801151b35a9bbfee01a79871a2217e12adcf68
maggi <margarita.herazo.i@gmail.com>
2024-09-24 10:33:09 +0200

    setting up about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,57 @@
-// src/components/About.js
 import React from 'react';
 
 function About() {
   return (
-    <section id="about">
-      <h2>About Us</h2>
-      <p>Learn more about our mission, vision, and the team behind our therapies.</p>
-      {/* Add more content about the team or the mission here */}
+    <section id="about" className="container">
+      <h2 className="text-center">About Me</h2>
+      <div className="row align-items-center">
+        <div className="col-md-4 text-center">
+          <img src="/assets/imgs/tre.jpg" alt="About Me" className="img-fluid about-image"/>
+        </div>
+        <div className="col-md-8"></div>
+      <div className="about-content">
+      <p>Im michaela born in tatatattaa and tatatta i arrived throu this path nannanaa</p>
+      </div>
+      <div className="bio-content">
+        <h3>Ausbildung:</h3>
+        <ul>
+          <li>1986-1994: Volksschule der Musik in Žacléř und Trutnov</li>
+          <li>1994-1999: Studium am Staatlichen Konservatorium in Prag, Fächer: Gesang, Klavier und Tanz</li>
+        </ul>
+
+        <h3>Frühere Projekte und Bands:</h3>
+        <ul>
+          <li>1995-1999: Mitglied der Hip-Hop-Band "Forbidden Fruit"</li>
+          <li>1996-1998: Arbeit beim Tschechischen Fernsehen/Radio</li>
+          <li>1998: Drum-and-Bass-Projekt "ATMOPRODUCKT" mit DJ, Live-Percussionist und Gesang</li>
+          <li>2004-2020: Drum'n'Bass-Projekt mit DJ und Gesang: "12 Inch Kid & Misa"</li>
+          <li>2004-2006: Chorleitung am Theater Jena für Stücke wie "JOHANNA", "ORESTIE" und "DER STURM"</li>
+          <li>2007-2010: Mitglied der Weltmusikband "LITTLE TONGUE VIBRATION ORCHESTRA" in Leipzig</li>
+          <li>2010-2020: Mitglied der Weltmusikband "ANNULUK" mit weltweiten Auftritten</li>
+          <li>2013: Internationaler Sommercamp-Acapella-Workshop</li>
+          <li>2013: Preisträger von Creole Mitteldeutschland</li>
+          <li>2013-2020: Leitung von Stimm- und Atem-Workshops in Jena und Leipzig</li>
+          <li>2013: CD-Veröffentlichung "USHNA"</li>
+          <li>2014: CD-Veröffentlichung "MALAM"</li>
+          <li>2014: Preisträger der Via Vut Indie Awards (best newcomer)</li>
+          <li>2016: CD-Veröffentlichung "B*A*M" - beautiful and massive</li>
+          <li>2020: EP-Veröffentlichung "MIHO"</li>
+        </ul>
+
+        <h3>Weiterbildungen und Erfahrungen:</h3>
+        <ul>
+          <li>2014: Beginn der Gestalttherapeutischen Aufstellungsarbeit und Integration in die körperbezogene Praxis</li>
+          <li>2022: Über 4 Monate Aufenthalt in Costa Rica</li>
+          <li>2023: Über 3 Monate Aufenthalt in Brasilien</li>
+          <li>2023-jetzt: Weiterbildung in Körperarbeit: Vagusnerv, Breathwork, Stimme, neurogenes Zittern und selbstgesteuertes Studium des Nervensystems basierend auf der Polyvagal-Theorie (Traumaheilung)</li>
+          <li>2023-2024: Ausbildung zum TRE® Provider (Tension and Trauma Releasing Exercises) nach David Berceli</li>
+          <li>Seit 05/2024: Arbeit in der Privatpraxis für ganzheitliche Medizin bei Frau Dr. med. Sabine Lodes/ Jena /Zöllnitz</li>
+        </ul>
+      </div>
+     </div>
     </section>
   );
 }
 
 export default About;
+
