d96fe6b7e51e05c3229fad5be5ce65411cf47579
maggi <margarita.herazo.i@gmail.com>
2024-09-24 09:42:01 +0200

    adding imgs adding styles concept imgs

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -14,22 +14,20 @@ function Concept() {
       <Link to="/fascialmanuvers">Fascial Manuvers</Link> and blabalbalaa
       <Link to="/tre">TRE</Link> and also in our vocal etc
         <Link to="/vocalbreath">Vocal Breath Therapy</Link> and lke this we heal
-      
-      </p>
-      </div>
-      
-      <div className="col-md-6">
-        <div className="image-item">
-          <img src="/assets/imgs/family.jfif" alt="Family Constellations" />
+        </p>
         </div>
-        <div className="image-item">
-          <img src="/assets/imgs/fascial.jfif" alt="Fascial Manuvers" />
+        <div className="col-md-6">
+          <div className="img-container">
+          <img src="/assets/imgs/tre.jpg" alt="TRE"className="img-fluid img-left" />
+        </div> 
+        <div className="img-container">
+          <img src="/assets/imgs/fascial.jfif" alt="Fascial Manuvers" className="img-fluid img-right"  />
         </div>
-        <div className="image-item">
-          <img src="/assets/imgs/tre.jpg" alt="TRE" />
+          <div className="img-container">
+          <img src="/assets/imgs/family.jfif" alt="Family Constellations" className="img-fluid img-left"/>
         </div>
-        <div className="image-item">
-          <img src="/assets/imgs/breath.jfif" alt="Vocal Breath Therapy" />
+        <div className="img-container">
+          <img src="/assets/imgs/breath.jfif" alt="Vocal Breath Therapy" className="img-fluid img-right" />
         </div>
       </div>
       </div>
