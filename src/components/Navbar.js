288ac0605e6a4ee91b2399a0eb02c06bf0ca39c1
maggi <margarita.herazo.i@gmail.com>
2024-10-15 12:49:29 +0200

    fixing navbar button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,9 +15,18 @@ function Navbar() {
                 <img src="/assets/imgs/Brain2.png" alt="Icon 3" className="navbar-img" />
               </div>
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+            <button 
+             className="navbar-toggler" 
+             type="button" 
+             data-bs-toggle="collapse" 
+             data-bs-target="#navbarNav" 
+             aria-controls="navbarNav" 
+             aria-expanded="false" 
+             aria-label="Toggle navigation"
+            >
+             <span className="navbar-toggler-icon"></span>
             </button>
+
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item"><Link className="nav-link" to="/vocalbreath">Atem & Stimmcoaching</Link></li>
