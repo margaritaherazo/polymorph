1be171b182800a1e80eea3e21a37b741c86b51b1
maggi <margarita.herazo.i@gmail.com>
2024-10-15 11:56:28 +0200

    familyconst  styles

--- src/pages/Familyconst.js
+++ src/pages/Familyconst.js
@@ -3,13 +3,25 @@ import React from 'react';
 
 function Familyconst() {
   return (
-    <div>
-      <h1>Gestalttherapeutische Aufstellungsarbeit</h1>
+    <div className="familyconst-container">
+    <section className="familyconst">
+      <div className="container">
+        <h1 className="text-center">Gestalttherapeutische Aufstellungsarbeit</h1>
+        <div className="row">
+          <div className="col-md-8">
       <p>Die gestalttherapeutische Aufstellungsarbeit basiert auf der Idee, dass wir als Individuen Teil eines größeren Ganzen sind, das uns beeinflusst – und hier spielt das morphogenetische Feld eine zentrale Rolle. Dieses Feld wird als energetisches oder informatorisches Netzwerk verstanden, das alle Lebewesen miteinander verbindet. Es speichert Erinnerungen, Erfahrungen und Dynamiken, die unbewusst unser Verhalten und unsere Beziehungen prägen.</p>
 
        <p>In einer Aufstellung geht es darum, diese Dynamiken, die sich oft hinter familiären, beruflichen oder persönlichen Problemen verbergen, sichtbar zu machen. Dabei werden Personen oder Symbole stellvertretend für Aspekte des Systems aufgestellt. Durch das morphogenetische Feld entsteht eine intuitive Verbindung zwischen den Repräsentanten und dem realen System, die es erlaubt, tieferliegende Konflikte, Spannungen oder unausgesprochene Gefühle zu erkennen.</p>
 
        <p>Indem die Positionen oder Beziehungen in der Aufstellung verändert werden, kann das morphogenetische Feld ebenfalls umstrukturiert werden. Dies löst alte Muster auf und schafft Raum für Heilung und neue Perspektiven. Der Klient spürt diese Veränderungen häufig als emotionale Entlastung und Klarheit, da sie im morphogenetischen Feld bereits eine neue Ordnung erfahren haben. Diese Veränderungen übertragen sich oft unmittelbar in den Alltag und führen zu spürbaren, positiven Veränderungen in den eigenen Beziehungen oder im inneren Erleben.</p>
+    </div>
+
+    <div className="col-md-4 text-center">
+              <img src="/assets/imgs/familyconst.jpg" alt="Gestalttherapeutische Aufstellungsarbeit" className="img-fluid familyconst-image" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
