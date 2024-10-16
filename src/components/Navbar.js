03d2454d084bf82a0d1e27360d4db2cd9b4afa4a
maggi <margarita.herazo.i@gmail.com>
2024-10-16 10:31:23 +0200

    adjusting Navbar fuction that collapse menu when is click a link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,55 +3,68 @@ import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 function Navbar() {
-  // State to manage the visibility of the navbar
   const [isOpen, setIsOpen] = useState(false);
 
-  // Function to toggle the navbar
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const handleNavLinkClick = () => setIsOpen(false); 
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link className="navbar-brand d-flex align-items-center" to="/">
-          MI°HO
-          <div className="navbar-images d-flex ml-2">
-            <img src="/assets/imgs/Heart2.png" alt="Icon 1" className="navbar-img" />
-            <img src="/assets/imgs/Lung2.png" alt="Icon 2" className="navbar-img" />
-            <img src="/assets/imgs/Brain2.png" alt="Icon 3" className="navbar-img" />
+    <header className="top-area">
+      <div className="header-area">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link className="navbar-brand d-flex align-items-center" to="/">
+              MI°HO
+              <div className="navbar-images d-flex ml-2">
+                <img src="/assets/imgs/Heart2.png" alt="Icon 1" className="navbar-img" />
+                <img src="/assets/imgs/Lung2.png" alt="Icon 2" className="navbar-img" />
+                <img src="/assets/imgs/Brain2.png" alt="Icon 3" className="navbar-img" />
+              </div>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={toggleMenu}
+              aria-controls="navbarNav"
+              aria-expanded={isOpen}
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/vocalbreath" onClick={handleNavLinkClick}>
+                    Atem & Stimmcoaching
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/fascialmanuvers" onClick={handleNavLinkClick}>
+                    Faszialle Manöver
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/tre" onClick={handleNavLinkClick}>
+                    TRE®
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/familyconst" onClick={handleNavLinkClick}>
+                    Gestalttherapeutische Aufstellungsarbeit
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact" onClick={handleNavLinkClick}>
+                    Kontakt
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={toggleNavbar} 
-          aria-expanded={isOpen} 
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/vocalbreath">Atem & Stimmcoaching</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/fascialmanuvers">Faszialle Manöver</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tre">TRE®</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/familyconst">Gestalttherapeutische Aufstellungsarbeit</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Kontakt</Link>
-            </li>
-          </ul>
-        </div>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 }
 
