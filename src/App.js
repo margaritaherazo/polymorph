eca3a381976479c69cb844f94040cb8c01ac9a20
maggi <margarita.herazo.i@gmail.com>
2024-10-16 11:12:23 +0200

    Added ScrollToTop component to automatically scroll to the top of each page on navigation.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import Familyconst from './pages/Familyconst';
 import Fascialmanuvers from './pages/Fascialmanuvers';
@@ -7,11 +8,22 @@ import Vocalbreath from './pages/Vocalbreath';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/familyconst" element={<Familyconst />} />
